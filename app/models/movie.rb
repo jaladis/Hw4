3bea7715ed4138c69d1b12561075dc39e3aa2139
sjaladi <sumalikajaladi@gmail.com>
2016-11-22 04:46:35 +0000

    working tests

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,12 @@
 class Movie < ActiveRecord::Base
 
-  attr_accessible :title, :rating, :description, :release_date
+  attr_accessible :title, :rating, :description, :release_date, :director
 
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
+  end
+  def self.find_similar_movies(m)
+  Movie.find_all_by_director(m.director)
   end
 end
 
